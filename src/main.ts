1477015a88b41d52555a8ccea6f874c29c9ed532
Héctor J. Vásquez <imvasen@gmail.com>
2022-01-23 21:29:02 -0600

    feat: log all http traffic

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ dotEnvConfig();
 
 import { NestFactory } from '@nestjs/core';
 
+import { HttpFilter, HttpInterceptor, Logger } from '@/common';
 import { AppModule } from '@/app.module';
 import * as config from '@/config';
-import { Logger } from '@/common';
 
 async function bootstrap() {
   const logLabel = 'Nest';
   const logger = new Logger(logLabel);
   const app = await NestFactory.create(AppModule, { logger });
+  await app.useGlobalInterceptors(new HttpInterceptor());
+  await app.useGlobalFilters(new HttpFilter());
   await app.setGlobalPrefix('api');
   await app.listen(config.PORT);
 }
